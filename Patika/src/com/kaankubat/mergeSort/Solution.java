466052eb7f04888f749c24d844bf08b87be08695
Kaan <92556652+kaankbt@users.noreply.github.com>
2022-02-04 22:10:51 +0300

    Merge Sort
    https://app.patika.dev/moduller/veri-yapilari-ve-algoritmalar/merge-sort-proje

---
+++ Patika/src/com/kaankubat/mergeSort/Solution.java
@@ -0,0 +1,31 @@
+package com.kaankubat.mergeSort;
+
+public class Solution {
+
+    /*
+
+Problem :
+
+[16,21,11,8,12,22] -> Merge Sort
+Yukarıdaki dizinin sort türüne göre aşamalarını yazınız.
+Big-O gösterimini yazınız.
+
+______________________________________________________________
+1) [16,21,11,8,12,22] -> Merge Sort Aşamaları:
+
+
+   [16,21,11] [8,12,22]
+
+ [16] [21,11] [8,12] [22]
+
+[16] [21] [11] [8] [12] [22]
+
+   [11,16,21] [8,12,22]
+
+    [8,11,12,16,21,22]
+
+______________________________________________________________
+
+2) Big O Notation: O(nlogn)
+     */
+}
