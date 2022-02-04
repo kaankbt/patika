56fca7a493b4b8444e5a8f6edf44e9eeb0e368f1
Kaan <170103074@ogr.alanya.edu.tr>
2022-02-05 00:00:36 +0300

    Vergi hesaplama

---
+++ Patika/src/com/kaankubat/taxCalculation/Solution.java
@@ -0,0 +1,26 @@
+package com.kaankubat.taxCalculation;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // write your code here
+        double tutar;
+        double kdv = 0.18;
+        Scanner input = new Scanner(System.in);
+        System.out.println("KDV oranı hesaplama");
+        System.out.println("-------------------");
+
+        System.out.print("Tutarı giriniz : ");
+        tutar = input.nextDouble();
+        if (tutar > 1000) {
+            kdv = 0.08;
+        }
+        System.out.println("Girilen Tutar :" + tutar);
+        tutar += tutar * kdv;
+        System.out.println("KDV Oranı : " + kdv);
+        System.out.println("KDV'li tutar : " + new DecimalFormat("#.#").format(tutar));
+    }
+}
