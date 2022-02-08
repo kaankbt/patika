70c6b2cd9bb09f7ea7048a62cfeb700b9620b23b
Kaan <170103074@ogr.alanya.edu.tr>
2022-02-08 21:18:39 +0300

    Hipotenüs

---
+++ Patika/src/com/kaankubat/Hypotenuse/Solution.java
@@ -0,0 +1,29 @@
+package com.kaankubat.Hypotenuse;
+
+import java.util.Scanner;
+
+public class Solution {
+        public static void main(String[] args) {
+
+            double edge1, edge2, edge3, u, area;
+
+
+            Scanner input1 = new Scanner(System.in);
+            System.out.print("Please write first edge : ");
+            edge1 = input1.nextDouble();
+
+            Scanner input2 = new Scanner(System.in);
+            System.out.print("Please write second edge : ");
+            edge2 = input2.nextDouble();
+
+            Scanner input3 = new Scanner(System.in);
+            System.out.print("Please write third edge : ");
+            edge3 = input3.nextDouble();
+
+            u = (edge1 + edge2 + edge3) / 2;
+            area = Math.sqrt(u * (u - edge1) * (u - edge2) * (u - edge3));
+
+            System.out.print("The area is : " + area);
+
+        }
+    }
